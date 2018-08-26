7b59f54e5788625fd88fdcbfdb5c1b17fd51b0a8
Dmitrii Balakhonskii <dmitrii.balakhonskii@zalando.de>
2018-08-26 10:12:19 +0200

    add lein-ancient and lein-changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,10 @@
 All notable changes to this project will be documented in this file. This change log follows the conventions of [keepachangelog.com](http://keepachangelog.com/).
 
 ## [Unreleased]
+### Added
+* Development-related:
+  * Use lein-ancient in CI to track outdated dependencies
+  * Use lein-changelog to automate changelog tasks
 
 ## 0.1.0 - 2018-08-21
 ### Added
--- project.clj
+++ project.clj
@@ -13,13 +13,16 @@
                  [crypto-equality "1.0.0"]
                  [org.bovinegenius/exploding-fish "0.3.6"]]
   :plugins [[lein-cloverage "1.0.9"]
-            [lein-shell "0.5.0"]]
+            [lein-shell "0.5.0"]
+            [lein-ancient "0.6.15"]
+            [lein-changelog "0.2.2"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.9.0"]]}}
   :deploy-repositories [["releases" :clojars]]
   :aliases {"update-readme-version" ["shell" "sed" "-i" "s/\\\\[me\\.dryewo\\\\/clj-github-app \"[0-9.]*\"\\\\]/[me\\.dryewo\\\\/clj-github-app \"${:version}\"]/" "README.md"]}
   :release-tasks [["shell" "git" "diff" "--exit-code"]
                   ["change" "version" "leiningen.release/bump-version"]
                   ["change" "version" "leiningen.release/bump-version" "release"]
+                  ["changelog" "release"]
                   ["update-readme-version"]
                   ["vcs" "commit"]
                   ["vcs" "tag"]
