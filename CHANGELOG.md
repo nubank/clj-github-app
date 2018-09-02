d2d1f3fdbd219c11af0bf1f2d99b7fce723b3b3b
Dmitrii Balakhonskii <dnomaid@gmail.com>
2018-09-03 01:58:18 +0200

    bump libs and plugins

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,16 +1,22 @@
-# Change Log
-All notable changes to this project will be documented in this file. This change log follows the conventions of [keepachangelog.com](http://keepachangelog.com/).
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](http://keepachangelog.com)
+and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
+
 
 ## [Unreleased]
 ### Added
 * Development-related:
-  * Use lein-ancient in CI to track outdated dependencies
+  * Use lein-ancient in CI to prevent outdated dependencies
   * Use lein-changelog to automate changelog tasks
 
-## 0.1.0 - 2018-08-21
+## 0.1.0 — 2018-08-21
 ### Added
 * Webhook payload signature checker with secure comparison.
 * API client with HTTP connection pool.
 * Access token manager with caching
+
 
 [Unreleased]: https://github.com/dryewo/clj-github-app/compare/0.1.0...HEAD
--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://github.com/dryewo/clj-github-app"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[clj-http "3.9.0"]
+  :dependencies [[clj-http "3.9.1"]
                  [org.clojure/core.cache "0.7.1"]
                  [com.auth0/java-jwt "3.4.0"]
                  [org.bouncycastle/bcprov-jdk15on "1.60"]
@@ -12,10 +12,10 @@
                  [ring/ring-codec "1.1.1"]
                  [crypto-equality "1.0.0"]
                  [org.bovinegenius/exploding-fish "0.3.6"]]
-  :plugins [[lein-cloverage "1.0.9"]
+  :plugins [[lein-cloverage "1.0.13"]
             [lein-shell "0.5.0"]
             [lein-ancient "0.6.15"]
-            [lein-changelog "0.2.2"]]
+            [lein-changelog "0.3.1"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.9.0"]]}}
   :deploy-repositories [["releases" :clojars]]
   :aliases {"update-readme-version" ["shell" "sed" "-i" "s/\\\\[me\\.dryewo\\\\/clj-github-app \"[0-9.]*\"\\\\]/[me\\.dryewo\\\\/clj-github-app \"${:version}\"]/" "README.md"]}
